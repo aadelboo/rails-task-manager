045d7aa5ed39f843e43ca0f977ce696b0bce089f
aadel bouzambou <abouzambou@gmail.com>
2022-01-15 18:07:05 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task.update(task_params)
+    task.update
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
-
-
-# En tant qu’utilisateur, je peux lister des tâches
-# En tant qu’utilisateur, je peux afficher les détails d’une tâche
-# En tant qu’utilisateur, je peux ajouter une nouvelle tâche
-# En tant qu’utilisateur, je peux éditer une tâche (la marquer comme terminée / mettre à jour le titre et les détails)
-# En tant qu’utilisateur, je peux supprimer une tâche
