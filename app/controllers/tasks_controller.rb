c1ec0e97f2044c5bc42d8547bd47c9742ccd7261
aadel bouzambou <abouzambou@gmail.com>
2022-01-15 12:12:09 +0100

    Add restaurant model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+class TasksController < ApplicationController
+
+end
+
+
+# En tant qu’utilisateur, je peux lister des tâches
+# En tant qu’utilisateur, je peux afficher les détails d’une tâche
+# En tant qu’utilisateur, je peux ajouter une nouvelle tâche
+# En tant qu’utilisateur, je peux éditer une tâche (la marquer comme terminée / mettre à jour le titre et les détails)
+# En tant qu’utilisateur, je peux supprimer une tâche
